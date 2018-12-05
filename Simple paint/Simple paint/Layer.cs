8e4533ec05d83c3f01415475e080901aa36f6478
tranquochung <45176783+hungcnpm@users.noreply.github.com>
2018-12-05 13:26:57 +0700

    Chức năng vẽ hình đơn giản
    Thêm class, thêm chức năng vẽ hình (hình tròn, hình elip, hình chữ nhật, đường thẳng), vẽ tự do, vẽ bằng pencil cho chương trình

---
+++ Simple paint/Simple paint/Layer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls.Primitives;
+using System.IO;
+
+namespace Simple_paint
+{
+
+    enum DrawType { nothing, pencil, brush, line, ellipse, rectangle, triangle, arrow, heart,  erase, text}
+
+    class Layer
+    {
+        public Canvas canvas;
+        public DrawType drawType;
+        public Layer(Canvas c)
+        {
+            drawType = DrawType.nothing;
+            canvas = c;
+        }
+        private ImageSource BitmapToImageSource(Bitmap bm)
+        {
+            System.Windows.Media.Imaging.BitmapSource b = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(bm.GetHbitmap(), IntPtr.Zero, System.Windows.Int32Rect.Empty, BitmapSizeOptions.FromWidthAndHeight(bm.Width, bm.Height));
+            return b;
+        }
+        public Bitmap CanvasToBitmap(Canvas cv)
+        {
+            Bitmap bm;
+            Rect bounds = VisualTreeHelper.GetDescendantBounds(cv);
+            double dpi = 96d;
+            RenderTargetBitmap renderBitmap = new RenderTargetBitmap((int)bounds.Width, (int)bounds.Height, dpi, dpi, System.Windows.Media.PixelFormats.Default);
+
+
+            DrawingVisual dv = new DrawingVisual();
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                VisualBrush vb = new VisualBrush(cv);
+                dc.DrawRectangle(vb, null, new Rect(new System.Windows.Point(), bounds.Size));
+            }
+            renderBitmap.Render(dv);
+
+            MemoryStream stream = new MemoryStream();
+            BitmapEncoder encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+            encoder.Save(stream);
+            bm = new System.Drawing.Bitmap(stream);
+            return bm;
+        }
+        public void DrawShape(ContentControl control)
+        {
+            canvas.Children.RemoveAt(canvas.Children.Count - 1);
+            RefreshCanvas();
+            canvas.Children.Add(control);
+        }
+        public void DrawShape(System.Windows.Shapes.Shape shape)
+        {
+            RefreshCanvas();
+            canvas.Children.Add(shape);
+        }
+
+        public void DrawCapture(Shape shape) //Vẽ hình được kéo ra lúc chưa nhả chuột
+        {
+            double[] dashes = { 2, 2 };
+            shape.StrokeDashArray = new System.Windows.Media.DoubleCollection(dashes);
+
+            canvas.Children.Add(shape);
+        }
+        public void RefreshCanvas()
+        {
+            System.Windows.Controls.Image img = new System.Windows.Controls.Image();
+            img.Width = canvas.ActualWidth;
+            img.Height = canvas.ActualHeight;
+            img.Source = BitmapToImageSource(CanvasToBitmap(canvas));
+            canvas.Children.Clear();
+            canvas.Children.Add(img);
+        }
+    }
+}
--- Simple paint/Simple paint/MainWindow.xaml.cs
+++ Simple paint/Simple paint/MainWindow.xaml.cs
@@ -12,8 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.IO;
 using Microsoft.Win32;
+using System.Windows.Controls.Primitives;
 
 namespace Simple_paint
 {
@@ -22,16 +22,317 @@ namespace Simple_paint
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Layer screen;
+        private Point mDown, mMove;
+        private bool capture;
+        private ContentControl curControl;
+        private System.Windows.Shapes.Shape curShape;
+        private Line curLine;
+        private Color penColor;
+        private double penThickness;
+        private PathGeometry pathGeometry;
+        private Path path;
+        private PathFigure pathFigure;
+        private string curFilePath;
+        private ContentControl selectedControl;
+        private TextBox focusedTextbox;
+        //text
+        private FontFamily fontFamily;
+        private double fontSize;
+        private FontStyle fontStyle;
+        private FontWeight fontWeight;
+        private TextDecorationCollection decoration;
+        private Color fontColor;
+        bool bold = false, italic = false, underlined = false;
+        float oldLineCount;
+
         public MainWindow()
         {
             InitializeComponent();
+            screen = new Layer(paintCanvas);
+            capture = false;
+            penColor = Colors.Black;
+            penThickness = 1;
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog() ;
+            OpenFileDialog ofd = new OpenFileDialog();
             ofd.ShowDialog();
         }
+        private void btnPencil_Click(object sender, RoutedEventArgs e)
+        {
+            screen.drawType = DrawType.pencil;
+            paintCanvas.Cursor = Cursors.Pen;
+        }
+
+        private void btnBrush_Click(object sender, RoutedEventArgs e)
+        {
+            screen.drawType = DrawType.brush;
+            penThickness = 3;
+            paintCanvas.Cursor = Cursors.Pen;
+        }
+        #region Draw simple picture
+        private void btnLine_Click(object sender, RoutedEventArgs e)
+        {
+            screen.drawType = DrawType.line;
+            paintCanvas.Cursor = Cursors.Cross;
+            
+        }
+
+        private void btnEllipse_Click(object sender, RoutedEventArgs e)
+        {
+            screen.drawType = DrawType.ellipse;
+            paintCanvas.Cursor = Cursors.Cross;
+
+        }
+
+        private void btnRectangle_Click(object sender, RoutedEventArgs e)
+        {
+            screen.drawType = DrawType.rectangle;
+            paintCanvas.Cursor = Cursors.Cross;
+        }
+        private void btnErazer_Click(object sender, RoutedEventArgs e)
+        {
+            screen.drawType = DrawType.erase;
+            paintCanvas.Cursor = Cursors.Arrow;
+        }
+        private void btnTriangle_Click(object sender, RoutedEventArgs e)
+        {
+            screen.drawType = DrawType.triangle;
+            paintCanvas.Cursor = Cursors.Cross;
+        }
+
+        private void btnArrow_Click(object sender, RoutedEventArgs e)
+        {
+            screen.drawType = DrawType.arrow;
+            paintCanvas.Cursor = Cursors.Cross;
+        }
+
+        private void btnHeart_Click(object sender, RoutedEventArgs e)
+        {
+            screen.drawType = DrawType.heart;
+            paintCanvas.Cursor = Cursors.Cross;
+        }
+        #endregion
+        #region MouseDown
+        private void paintCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            mDown = e.GetPosition(this.paintCanvas);
+            capture = true;
+            if (screen.drawType == DrawType.brush || screen.drawType == DrawType.pencil || screen.drawType == DrawType.erase)
+            {
+                pathGeometry = new PathGeometry();
+                pathFigure = new PathFigure();
+                pathFigure.StartPoint = mDown;
+                pathFigure.IsClosed = false;
+                pathGeometry.Figures.Add(pathFigure);
+                path = new Path();
+                path.Stroke = new SolidColorBrush(penColor);
+                if (screen.drawType == DrawType.erase)
+                {
+                    path.Stroke = new SolidColorBrush(Colors.White);
+                }
+                if (screen.drawType == DrawType.brush || screen.drawType == DrawType.erase)
+                {
+                    path.StrokeThickness = penThickness;
+                }
+                else if (screen.drawType == DrawType.pencil)
+                {
+                    path.StrokeThickness = 1;
+                }
+                path.Data = pathGeometry;
+                screen.DrawShape(path);
+            }
+        }
+
        
+        #endregion
+
+        #region MouseMove
+        private void paintCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            mMove = e.GetPosition(this.paintCanvas);
+            bool addShape = false;
+            if ((screen.drawType == DrawType.ellipse || screen.drawType == DrawType.rectangle || screen.drawType == DrawType.triangle || screen.drawType == DrawType.arrow || screen.drawType == DrawType.heart) && capture)
+            {
+
+                if (curShape == null)
+                {
+
+                    if (screen.drawType == DrawType.ellipse)
+                    {
+                        curShape = new Ellipse();
+                    }
+                    else if (screen.drawType == DrawType.rectangle)
+                    {
+                        curShape = new Rectangle();
+                    }
+                    else if (screen.drawType == DrawType.triangle)
+                    {
+
+                        curShape = new Triangle();
+                        ((Triangle)curShape).Start = mDown;
+                    }
+                    else if (screen.drawType == DrawType.arrow)
+                    {
+
+                        curShape = new Arrow();
+                        ((Arrow)curShape).Start = mDown;
+                    }
+                    else
+                    {
+                        curShape = new Heart();
+                        ((Heart)curShape).Start = mDown;
+                    }
+                    addShape = true;
+                    curShape.StrokeThickness = penThickness;
+                    curShape.Stroke = new SolidColorBrush(penColor);
+                }
+
+                if (mMove.X <= mDown.X && mMove.Y <= mDown.Y)  //Góc phần tư thứ nhất
+                {
+                    curShape.Margin = new Thickness(mMove.X, mMove.Y, 0, 0);
+                }
+                else if (mMove.X >= mDown.X && mMove.Y <= mDown.Y)
+                {
+                    curShape.Margin = new Thickness(mDown.X, mMove.Y, 0, 0);
+                }
+                else if (mMove.X >= mDown.X && mMove.Y >= mDown.Y)
+                {
+                    curShape.Margin = new Thickness(mDown.X, mDown.Y, 0, 0);
+                }
+                else if (mMove.X <= mDown.X && mMove.Y >= mDown.Y)
+                {
+                    curShape.Margin = new Thickness(mMove.X, mDown.Y, 0, 0);
+                }
+
+                curShape.Width = Math.Abs(mMove.X - mDown.X);
+                curShape.Height = Math.Abs(mMove.Y - mDown.Y);
+
+
+                if (addShape)
+                {
+                    screen.DrawCapture(curShape);
+                }
+            }
+            else if (screen.drawType == DrawType.line && capture)
+            {
+                if (curLine == null)
+                {
+                    curLine = new Line();
+                    addShape = true;
+                }
+                curLine.X1 = mDown.X;
+                curLine.Y1 = mDown.Y;
+                curLine.X2 = mMove.X;
+                curLine.Y2 = mMove.Y;
+                curLine.StrokeThickness = penThickness;
+                curLine.Stroke = new SolidColorBrush(penColor);
+                if (addShape)
+                {
+                    screen.DrawCapture(curLine);
+
+                }
+            }
+            else if ((screen.drawType == DrawType.brush || screen.drawType == DrawType.pencil || screen.drawType == DrawType.erase) && capture)
+            {
+                LineSegment ls = new LineSegment();
+                ls.Point = mMove;
+                pathFigure.Segments.Add(ls);
+            }
+        }
+        #endregion
+        #region MouseLeftButtonUp
+        private void paintCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            capture = false;
+            if (curShape != null)
+            {
+                if (screen.drawType == DrawType.ellipse || screen.drawType == DrawType.rectangle || screen.drawType == DrawType.triangle || screen.drawType == DrawType.arrow || screen.drawType == DrawType.heart)
+                {
+                    Shape temp;
+                    if (screen.drawType == DrawType.ellipse)
+                    {
+                        temp = new Ellipse();
+                    }
+                    else if (screen.drawType == DrawType.rectangle)
+                    {
+                        temp = new Rectangle();
+                    }
+                    else if (screen.drawType == DrawType.triangle)
+                    {
+                        temp = new Triangle();
+                    }
+                    else if (screen.drawType == DrawType.arrow)
+                    {
+                        temp = new Arrow();
+                    }
+                    else
+                    {
+                        temp = new Heart();
+                    }
+                    temp.Stroke = new SolidColorBrush(penColor);
+                    temp.StrokeThickness = penThickness;
+                    curControl = new ContentControl();
+                    temp.IsHitTestVisible = true;
+                    if (screen.drawType == DrawType.triangle)
+                    {
+                        ((Triangle)temp).Start = ((Triangle)curShape).Start;
+                        temp.Width = curShape.Width;
+                        temp.Height = curShape.Height;
+                    }
+                    if (screen.drawType == DrawType.arrow)
+                    {
+                        ((Arrow)temp).Start = ((Arrow)curShape).Start;
+                        temp.Width = curShape.Width;
+                        temp.Height = curShape.Height;
+                    }
+                    if (screen.drawType == DrawType.heart)
+                    {
+                        ((Heart)temp).Start = ((Heart)curShape).Start;
+                        temp.Width = curShape.Width;
+                        temp.Height = curShape.Height;
+                    }
+                    Canvas.SetLeft(curControl, curShape.Margin.Left);
+                    Canvas.SetTop(curControl, curShape.Margin.Top);
+                    curControl.Width = curShape.Width;
+                    curControl.Height = curShape.Height;
+                    curControl.Content = temp;
+                    curControl.Background = new SolidColorBrush(Colors.White);
+                    screen.DrawShape(curControl);
+                }
+                curShape = null;
+            }
+            else if (screen.drawType == DrawType.line && curLine != null)
+            {
+                Line line = new Line();
+                line.Stroke = new SolidColorBrush(penColor);
+                line.StrokeThickness = penThickness;
+                line.X1 = curLine.X1;
+                line.X2 = curLine.X2;
+                line.Y1 = curLine.Y1;
+                line.Y2 = curLine.Y2;
+                screen.DrawShape(line);
+                curLine = null;
+            }
+
+        }
+        #endregion
+        private void SimplePaint_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void SimplePaint_Loaded(object sender, RoutedEventArgs e)
+        {
+            fontSize = 8;
+            fontColor = Colors.Black;
+            fontStyle = FontStyles.Normal;
+            fontWeight = FontWeights.Normal;
+            decoration = null;
+
+        }
+
     }
 }
